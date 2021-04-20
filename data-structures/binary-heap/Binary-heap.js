e3dd791f928aef370c789476e08cd64a60f6cfc7
Khan Mohsin <m90khan@gmail.com>
2021-04-20 08:51:28 +0500

    heap

--- data-structures/binary-heap/Binary-heap.js
+++ data-structures/binary-heap/Binary-heap.js
@@ -2,13 +2,7 @@
 
 // Implement the following functions on the MaxBinaryHeap/MinBinaryHeap class
 
-// insert
-// This function should insert a node in a binary heap.
-// Make sure to re-order the heap after insertion if necessary.
-
-// extractMax/Min
-// This function should remove the root node in a binary heap.
-// Make sure to re-order the heap after removal if necessary.
+//
 
 // heapify
 // This function should convert array into a max/min heap.
@@ -33,11 +27,11 @@ class BinaryHeap {
   }
 
   getLeftChildIndex(parentIndex) {
-    return (2 * parentIndex) + 1;
+    return 2 * parentIndex + 1;
   }
 
   getRightChildIndex(parentIndex) {
-    return (2 * parentIndex) + 2;
+    return 2 * parentIndex + 2;
   }
 
   getParentIndex(childIndex) {
@@ -47,12 +41,39 @@ class BinaryHeap {
   getItem(index) {
     return this.values[index];
   }
+  /*
+- Insert
+ This function should insert a node in a binary heap.
+ Make sure to re-order the heap after insertion if necessary.
 
+INSERT PSEUDOCODE
+
+Push the value into the values property on the heap
+Bubble Up:
+Create a variable called index which is the length of the values property - 1
+Create a variable called parentIndex which is the floor of (index-1)/2
+Keep looping as long as the values element at the parentIndex is less than the values element at the child index
+Swap the value of the values element at the parentIndex with the value of the element property at the child index
+Set the index to be the parentIndex, and start over!
+*/
   insert(value) {
     this.values.push(value);
     this.bubbleUp();
 
     return this;
+  }
+  bubbleUp(idx = this.values.length - 1) {
+    let index = idx;
+    const value = this.getItem(index);
+
+    while (index > 0) {
+      const parentIndex = this.getParentIndex(index);
+
+      if (this.compare(this.getItem(parentIndex), value)) break;
+
+      this.swap(index, parentIndex);
+      index = parentIndex;
+    }
   }
 
   extract() {
@@ -81,7 +102,24 @@ class BinaryHeap {
 
     return indexes;
   }
+  /*
+REMOVING
 
+extractMax/Min
+ This function should remove the root node in a binary heap.
+ Make sure to re-order the heap after removal if necessary.
+
+ Swap the first value in the values property with the last one
+Pop from the values property, so you can return the value at the end.
+Have the new root "sink down" to the correct spot...​
+Your parent index starts at 0 (the root)
+Find the index of the left child: 2 * index + 1 (make sure its not out of bounds)
+Find the index of the right child: 2*index + 2 (make sure its not out of bounds)
+If the left or right child is greater than the element...swap. If both left and right children are larger, swap with the largest child.
+The child index you swapped to now becomes the new parent index.  
+Keep looping and swapping until neither child is larger than the element.
+Return the old root!
+*/
   remove(value, getValueFn = this.getItem) {
     const numberItems = this.find(value, getValueFn).length;
 
@@ -141,25 +179,11 @@ class BinaryHeap {
 
     while (this.values.length) {
       sortedArray.push(this.extract());
-    };
+    }
 
     this.values = [...valuesCopy];
 
     return sortedArray;
-  }
-
-  bubbleUp(idx = this.values.length - 1) {
-    let index = idx;
-    const value = this.getItem(index);
-
-    while (index > 0) {
-      const parentIndex = this.getParentIndex(index);
-
-      if (this.compare(this.getItem(parentIndex), value)) break;
-
-      this.swap(index, parentIndex);
-      index = parentIndex;
-    }
   }
 
   sinkDown(idx = 0) {
@@ -172,15 +196,23 @@ class BinaryHeap {
 
       if (leftChildIndex >= length && leftChildIndex >= length) break;
 
-      if (rightChildIndex >= length &&
-        this.compare(this.getItem(index), this.getItem(leftChildIndex))) break;
+      if (
+        rightChildIndex >= length &&
+        this.compare(this.getItem(index), this.getItem(leftChildIndex))
+      )
+        break;
 
-      if (this.compare(this.getItem(index), this.getItem(leftChildIndex)) &&
-        this.compare(this.getItem(index), this.getItem(rightChildIndex))) break;
+      if (
+        this.compare(this.getItem(index), this.getItem(leftChildIndex)) &&
+        this.compare(this.getItem(index), this.getItem(rightChildIndex))
+      )
+        break;
 
-      const swapIndex = rightChildIndex >= length ||
-      this.compare(this.getItem(leftChildIndex), this.getItem(rightChildIndex))
-        ? leftChildIndex : rightChildIndex;
+      const swapIndex =
+        rightChildIndex >= length ||
+        this.compare(this.getItem(leftChildIndex), this.getItem(rightChildIndex))
+          ? leftChildIndex
+          : rightChildIndex;
 
       this.swap(index, swapIndex);
       index = swapIndex;
@@ -192,8 +224,10 @@ class BinaryHeap {
   }
 
   swap(indexOne, indexTwo) {
-    [this.values[indexOne], this.values[indexTwo]] =
-      [this.values[indexTwo], this.values[indexOne]];
+    [this.values[indexOne], this.values[indexTwo]] = [
+      this.values[indexTwo],
+      this.values[indexOne],
+    ];
   }
 
   compare(firstItem, secondItem) {
