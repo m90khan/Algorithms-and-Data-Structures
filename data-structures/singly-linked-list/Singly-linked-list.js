754ac134fb7bb3dba80fc0f230659a7131648a29
Khan Mohsin <m90khan@gmail.com>
2021-05-06 23:06:56 +0500

    linked list

--- data-structures/singly-linked-list/Singly-linked-list.js
+++ data-structures/singly-linked-list/Singly-linked-list.js
@@ -23,7 +23,16 @@ Linked Lists consist of nodes, and each node has a value and a pointer to anothe
 */
 
 const SinglyLinkedListNode = require('./Singly-linked-list-node');
+/*
 
+ class SinglyLinkedListNode {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+};
+
+*/
 class SinglyLinkedList {
   constructor() {
     this.head = null;
@@ -352,3 +361,79 @@ if (module.parent) {
   singlyLinkedList.print(); // [ 10, 5, 25, 20, 100 ]
   console.log(singlyLinkedList.find(25, true)); // 2
 }
+
+/*
+function LinkedList() {
+  this.head = null;
+  this.tail = null;
+}
+
+function Node(value, next, prev) {
+  this.value = value;
+  this.next = next;
+  this.prev = prev;
+}
+
+LinkedList.prototype.addToHead = function(value) {
+  var newNode = new Node(value, this.head, null);
+  if (this.head) this.head.prev = newNode;
+  else this.tail = newNode;
+  this.head = newNode;
+};
+
+LinkedList.prototype.addToTail = function(value) {
+  var newNode = new Node(value, null, this.tail);
+  if (this.tail) this.tail.next = newNode;
+  else this.head = newNode;
+  this.tail = newNode;
+};
+
+LinkedList.prototype.removeHead = function() {
+  if (!this.head) return null;
+  var val = this.head.value;
+  this.head = this.head.next;
+  if (this.head) this.head.prev = null;
+  else this.tail = null;
+  return val;
+};
+
+LinkedList.prototype.removeTail = function() {
+  if (!this.tail) return null;
+  var val = this.tail.value;
+  this.tail = this.tail.prev;
+  if (this.tail) this.tail.next = null;
+  else this.head = null;
+  return val;
+};
+
+LinkedList.prototype.search = function(searchValue) {
+  var currentNode = this.head;
+  while (currentNode) {
+    if (currentNode.value === searchValue) return currentNode.value;
+    currentNode = currentNode.next;
+  } 
+  return null;
+};
+
+LinkedList.prototype.indexOf = function(value) {
+  var indexes = [];
+  var currentIndex = 0;
+  var currentNode = this.head;
+  while(currentNode) {
+    if (currentNode.value === value) indexes.push(currentIndex);
+    currentNode = currentNode.next;
+    currentIndex++;
+  }
+  return indexes;
+};
+
+
+var myLL = new LinkedList();
+
+myLL.addToHead(123);
+myLL.addToHead(70);
+myLL.addToHead('hello');
+myLL.addToTail(19);
+myLL.addToTail('world');
+myLL.addToTail(20);
+*/
